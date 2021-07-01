edc4060ebdf2032cff4ed7352d84fa5b98203988
bogdan24bondar@gmail.com <bogdan24bondar@gmail.com>
2021-07-01 23:39:21 +0300

    Add NativeWin32 class

---
+++ VirtualKeyboardLibrary/NativeWin32.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualKeyboardLibrary
+{
+    public class NativeWin32
+    {
+        #region Consts
+
+        public const int WS_EX_NOACTIVE = 0x08000000;
+        public const int GWL_EXSTYLE = -20;
+        public const int MAPVK_VK_TO_VSC = 0;
+        public const uint KLF_SETFORPROCESS = 0x00000100;
+
+        #endregion
+
+        #region Structs
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct INPUT
+        {
+            public uint type;
+            public InputUnion U;
+            public static int Size
+            {
+                get { return Marshal.SizeOf(typeof(INPUT)); }
+            }
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct InputUnion
+        {
+            [FieldOffset(0)]
+            public MOUSEINPUT mi;
+            [FieldOffset(0)]
+            public KEYBDINPUT ki;
+            [FieldOffset(0)]
+            public HARDWAREINPUT hi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public MouseEventDataXButtons mouseData;
+            public MOUSEEVENTF dwFlags;
+            public uint time;
+            public UIntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct KEYBDINPUT
+        {
+            public VKeyCode wVk;
+            public ushort wScan;
+            public KEYEVENTF dwFlags;
+            public int time;
+            public UIntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct HARDWAREINPUT
+        {
+            public int uMsg;
+            public short wParamL;
+            public short wParamH;
+        }
+
+        #endregion
+
+        #region Enums
+
+        [Flags]
+        public enum MouseEventDataXButtons : uint
+        {
+            Nothing = 0x00000000,
+            XBUTTON1 = 0x00000001,
+            XBUTTON2 = 0x00000002
+        }
+
+        [Flags]
+        public enum MOUSEEVENTF : uint
+        {
+            ABSOLUTE = 0x8000,
+            HWHEEL = 0x01000,
+            MOVE = 0x0001,
+            MOVE_NOCOALESCE = 0x2000,
+            LEFTDOWN = 0x0002,
+            LEFTUP = 0x0004,
+            RIGHTDOWN = 0x0008,
+            RIGHTUP = 0x0010,
+            MIDDLEDOWN = 0x0020,
+            MIDDLEUP = 0x0040,
+            VIRTUALDESK = 0x4000,
+            WHEEL = 0x0800,
+            XDOWN = 0x0080,
+            XUP = 0x0100
+        }
+
+        [Flags]
+        public enum KEYEVENTF : uint
+        {
+            KEYDOWN = 0x0000,
+            EXTENDEDKEY = 0x0001,
+            KEYUP = 0x0002,
+            SCANCODE = 0x0008,
+            UNICODE = 0x0004
+        }
+
+        #endregion
+
+        #region Methods
+
+        [DllImport("user32.dll")]
+        internal static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        public static extern int MapVirtualKey(uint uCode, uint uMapType);
+
+        [DllImport("user32.dll")]
+        static internal extern uint GetKeyboardLayoutList(int nBuff, [Out] IntPtr[] lpList);
+
+        [DllImport("user32.dll")]
+        static internal extern uint ActivateKeyboardLayout(IntPtr hkl, uint flags);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int ToUnicodeEx(uint virtualKeyCode, uint scanCode,
+            byte[] keyboardState,
+            [Out, MarshalAs(UnmanagedType.LPWStr, SizeParamIndex = 4)] StringBuilder receivingBuffer,
+            int bufferSize,
+            uint flags,
+            IntPtr dwhkl);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern ushort GetKeyboardLayout([In] int idThread);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetWindowThreadProcessId([In] IntPtr hWnd, [Out, Optional] IntPtr lpdwProcessId);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetForegroundWindow();
+
+        #endregion
+
+
+    }
+}
