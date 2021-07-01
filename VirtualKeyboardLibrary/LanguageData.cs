6b6b3876eec1bcc320ddde354d20b621f79649ff
bogdan24bondar@gmail.com <bogdan24bondar@gmail.com>
2021-07-01 23:44:21 +0300

    Add LanguageData class

---
+++ VirtualKeyboardLibrary/LanguageData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualKeyboardLibrary
+{
+    public class LanguageData
+    {
+        public int Number { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
