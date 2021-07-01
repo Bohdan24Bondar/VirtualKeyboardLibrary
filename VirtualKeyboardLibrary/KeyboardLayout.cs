9893cd09822aace0d08fc1f3ff7ba0400f2a0764
bogdan24bondar@gmail.com <bogdan24bondar@gmail.com>
2021-07-01 23:45:53 +0300

    Add KeyboardLayout enum

---
+++ VirtualKeyboardLibrary/KeyboardLayout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualKeyboardLibrary
+{
+    public enum KeyboardLayout
+    {
+        Full = 0,
+        Alfbet = 1,
+        Numpad = 2
+    }
+}
